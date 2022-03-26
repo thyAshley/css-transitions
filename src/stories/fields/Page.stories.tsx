db3a5237c6272b0666511511351f8382295dddaf
thyashley <thy.ashley@gmail.com>
2022-03-26 16:08:06 +0800

    initial commit

---
+++ src/stories/fields/Page.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { within, userEvent } from "@storybook/testing-library";
+import { Page } from "./Page";
+
+export default {
+  title: "Example/Page",
+  component: Page,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof Page>;
+
+const Template: ComponentStory<typeof Page> = (args) => <Page />;
+
+export const Temp = Template.bind({});
